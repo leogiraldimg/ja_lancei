d86be10f512903b1a8569cecbc8254155c51fc29
leogiraldimg <leogiraldimg@usp.br>
2020-01-31 09:25:14 -0300

    Correcting managers migration

--- src/ja_lancei/db/migrate/20200116014147_create_managers.rb
+++ src/ja_lancei/db/migrate/20200116014147_create_managers.rb
@@ -1,6 +1,6 @@
 class CreateManagers < ActiveRecord::Migration[5.2]
   def change
-    create_table :users do |t|
+    create_table :managers do |t|
       t.string :email, null: false, default: ''
       t.string :first_name, null: false, default: ''
       t.string :last_name, null: false, default: ''
